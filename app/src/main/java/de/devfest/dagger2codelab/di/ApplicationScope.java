97ac39e41b751691f52f5191a0a934fe1e30f0db
ferrolb <ferrol.blackmon@gmail.com>
2018-05-25 22:27:34 -0400

    Create application scope

---
+++ app/src/main/java/de/devfest/dagger2codelab/di/ApplicationScope.java
@@ -0,0 +1,12 @@
+package de.devfest.dagger2codelab.di;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ApplicationScope {
+
+}
